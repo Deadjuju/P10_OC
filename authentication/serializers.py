c695bcccd719c90cf99af17f7da7ce8f43731224
Julien Garcia <julien.garcia-lp@laposte.net>
2022-06-03 10:39:52 +0200

    save a user

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -1,6 +1,12 @@
+from django.contrib.auth.hashers import make_password
+from rest_framework import serializers
 from rest_framework.serializers import ModelSerializer
 
-from authentication.models import User
+# from authentication.models import User
+from django.contrib.auth import get_user_model
+
+
+User = get_user_model()
 
 
 class UserSerializer(ModelSerializer):
@@ -11,3 +17,28 @@ class UserSerializer(ModelSerializer):
         extra_kwargs = {
             'password': {'write_only': True},
         }
+
+    def create(self, validated_data):
+        print("SERIALIZOR!!!")
+        email = validated_data['email']
+        first_name = validated_data['first_name']
+        last_name = validated_data['last_name']
+        password = validated_data["password"]
+        user = User.objects.create(**validated_data)  # saving user object
+        # user = User.objects.create_user(**validated_data)  # saving user object
+        user.set_password(validated_data["password"])
+        user.save()
+        print("SAVE!!!")
+        return user
+
+        # def create(self, validated_data):
+        #     print("COUCOU")
+        #     user = User(
+        #         email=validated_data['email'],
+        #         first_name=validated_data['first_name'],
+        #         last_name=validated_data['last_name'],
+        #     )
+        #     user.set_password(validated_data["password"])
+        #     user.save()
+        #     print("bye!")
+        #     return user
--- authentication/views.py
+++ authentication/views.py
@@ -1,7 +1,14 @@
+from rest_framework import status
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
-from authentication.models import User
+# from authentication.models import User
+from django.contrib.auth import get_user_model
+
 from authentication.serializers import UserSerializer
+
+
+User = get_user_model()
 
 
 class UserViewset(ModelViewSet):
@@ -9,3 +16,20 @@ class UserViewset(ModelViewSet):
 
     def get_queryset(self):
         return User.objects.all()
+
+    def create(self, request, *args, **kwargs):
+        print("COUCOU")
+        data = {
+            "email": request.POST.get('email'),
+            "first_name": request.POST.get('first_name'),
+            "last_name": request.POST.get('last_name'),
+            "password": request.POST.get('password')
+        }
+        serializer = self.serializer_class(data=data)
+        print(f"USER: {serializer}")
+        if serializer.is_valid():
+            serializer.save()
+            return Response(data=serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
