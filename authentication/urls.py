7f66e4450f1c0238ec2c03d9166e16a74c7d6fcf
Julien Garcia <julien.garcia-lp@laposte.net>
2022-06-04 18:58:05 +0200

    clean up urls

--- authentication/urls.py
+++ authentication/urls.py
@@ -8,10 +8,11 @@ from authentication.views import UserViewset
 app_name = 'authentication'
 
 router = routers.SimpleRouter()
-router.register('user', UserViewset, basename='user')
+router.register('users', UserViewset, basename='user')
+router.register('signup', UserViewset, basename='signup')
 
 urlpatterns = [
-    path('api/token/', TokenObtainPairView.as_view(), name='obtain_tokens'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='refresh_token'),
-    path('', include(router.urls))
+    path('login/', TokenObtainPairView.as_view(), name='login'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='refresh_token'),
+    path('', include(router.urls)),
 ]
