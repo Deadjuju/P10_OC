78de6984988ca8fe1eafc0b497a8109c83cde3ce
Julien Garcia <julien.garcia-lp@laposte.net>
2022-06-08 09:24:45 +0200

    removed unnecessary overhead on 'perform_delete() - contributor'

--- api/views.py
+++ api/views.py
@@ -100,15 +100,11 @@ class ContributorViewset(ModelViewSet):
 
     def destroy(self, request, *args, **kwargs):
         instance = self.get_object()
-        print(f"Instance: {instance}")
         self.perform_destroy(instance)
         return Response({
             "message": "Contributor deleted successfully"
         },
             status=status.HTTP_200_OK)
-
-    def perform_destroy(self, instance):
-        instance.delete()
 
 
 class IssueViewset(ModelViewSet):
