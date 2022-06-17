22f737ad10417e99311b0ac3b9643efed008c57c
Julien Garcia <julien.garcia-lp@laposte.net>
2022-06-17 09:35:10 +0200

    access token configuration

--- SoftDesk/settings.py
+++ SoftDesk/settings.py
@@ -42,7 +42,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=100),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
 
 }
